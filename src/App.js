7e600bfbfc4dea9adbc4fc5592c5e8868d319941
Ebenezer <Oluwatadeebenezer@gmail.com>
2023-08-17 16:44:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {books} from "./books"
+import Book from "./Book"
 
-function App() {
+const BookList = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>amazon best selling </h1>
+    <section className="booklist">
+    {books.map((book) => {
+      return  <Book {...book} key={book.id}/>
+    })}
+    </section>
+    </>
   );
-}
+};
 
-export default App;
+
+
+export { BookList };
---
+++ src/books.js
@@ -0,0 +1,26 @@
+import book1 from "./images/book1_.jpg";
+import book2 from "./images/book2.jpg";
+import book3 from "./images/book3.jpg"
+
+export const books = [
+    {
+    author: "Alex Michaelides",
+    title: "The Silent Patient",
+    image: book1,
+    id: 1,
+   },
+   {
+    author: "Taylor Jenkins Reid",
+    title: "The Seven Husbands of Evelyn Hugo", 
+    image: book2,
+    id: 2,
+   },
+   {
+    author: "Charlie Donlea",
+    title: "The Girl Who Was Taken",
+    image: book3,
+    id:3,
+   },
+  ]
+  
+  
